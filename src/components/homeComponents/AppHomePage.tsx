aa7cacfaa48d908debc0fcb43b54f336d199d419
LuizHenriqueDelPosoVilalva <luizgarciaevilalva@gmail.com>
2023-10-07 17:05:10 -0400

    concertado imagem logo

--- src/components/homeComponents/AppHomePage.tsx
+++ src/components/homeComponents/AppHomePage.tsx
@@ -1,14 +1,12 @@
 import TopBar from "../commons/TopBar"
 import LogoAndMenu from "./UI/LogoAndMenu"
 
-import { Flex } from "@chakra-ui/react"
-
 function AppHomePage () {
     return (
-        <Flex flexDirection="column" w="100%">
+        <>
             <TopBar />
             <LogoAndMenu />
-        </Flex>
+        </>
     )
 }
 
--- src/components/homeComponents/UI/LogoAndMenu.tsx
+++ src/components/homeComponents/UI/LogoAndMenu.tsx
@@ -10,7 +10,7 @@ import DrawerMenu from "../../commons/DrawerMenu"
 function LogoAndMenu () {
     return(
         <Flex pl="2%" pt="2%" pr="2%" maxH="250px" className={styles.container}>      
-            <Box ml="5%"  mr="5%" boxSize="sm" className={styles.logo}>
+            <Box ml="5%"  mr="5%" className={styles.logo} mb="5%">
                 <Animation translateX="-100%" seconds='0.5s'>
                     <Image 
                         src='https://static.wixstatic.com/media/b877e3_757cb6a4f0394b868ab8aeb35fa1e9e3~mv2.jpeg/v1/fill/w_202,h_150,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/WhatsApp%20Image%202022-10-28%20at%2016_20_28.jpeg' 
